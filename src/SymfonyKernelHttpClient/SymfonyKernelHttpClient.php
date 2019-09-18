2ecf8540aa1b5206bc0a05d5105505d84c29fce3
Adam Quaile <adamquaile@gmail.com>
2019-09-18 21:13:04 +0100

    Made SymfonyKernelHttpClient implement HttpAsyncClient

--- src/SymfonyKernelHttpClient/SymfonyKernelHttpClient.php
+++ src/SymfonyKernelHttpClient/SymfonyKernelHttpClient.php
@@ -2,8 +2,13 @@
 
 namespace Outstack\Components\SymfonyKernelHttpClient;
 
+use Http\Client\Exception;
 use Http\Client\Exception\HttpException;
+use Http\Client\HttpAsyncClient;
 use Http\Client\HttpClient;
+use Http\Promise\FulfilledPromise;
+use Http\Promise\Promise;
+use Http\Promise\RejectedPromise;
 use Outstack\Components\HttpInterop\Psr7\ServerEnvironmentRequestFactory;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -12,7 +17,7 @@ use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
-class SymfonyKernelHttpClient implements HttpClient
+class SymfonyKernelHttpClient implements HttpClient, HttpAsyncClient
 {
     /**
      * @var HttpKernelInterface
@@ -79,4 +84,21 @@ class SymfonyKernelHttpClient implements HttpClient
         return $psrResponse;
     }
 
+    /**
+     * Sends a PSR-7 request in an asynchronous way.
+     *
+     * Exceptions related to processing the request are available from the returned Promise.
+     *
+     * @return Promise Resolves a PSR-7 Response or fails with an Http\Client\Exception.
+     *
+     * @throws \Exception If processing the request is impossible (eg. bad configuration).
+     */
+    public function sendAsyncRequest(RequestInterface $request)
+    {
+        try {
+            return new FulfilledPromise($this->sendRequest($request));
+        } catch (Exception $exception) {
+            return new RejectedPromise($exception);
+        }
+    }
 }
